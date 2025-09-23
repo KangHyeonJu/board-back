382d71465f55fd3e4b554e3197bfd098d28e20e0
KabgHyeonJu <khj010926@gmail.com>
2025-09-23 15:13:10 +0900

    Initial commit

---
+++ src/test/java/com/hyeonju/boardback/BoardBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonju.boardback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
